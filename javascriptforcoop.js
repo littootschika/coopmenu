b735940e7e81c70756ae8a517194901272e3282f
diamondstorm-leaderofchickenresistance <143503714+diamondstorm-leaderofchickenresistance@users.noreply.github.com>
2023-09-19 11:28:09 +0800

    Update javascriptforcoop.js

--- javascriptforcoop.js
+++ javascriptforcoop.js
@@ -1,7 +1,8 @@
 function ChangeBackground() {
 const images = [
   "docs/assets/old fashion chicken.png",
-  "docs/assets/tschika fetch the chips.png"
+  "docs/assets/tschika fetch the chips.png",
+  "docs/assets/justice for shrimp.png"
 ];
 var randomIndex = Math.floor(Math.random() * images.length);
 var selectedImage = images[randomIndex];
