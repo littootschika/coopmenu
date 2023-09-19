6a46ecc27aaff36270ccfd4c5c88e0040d23d03a
diamondstorm-leaderofchickenresistance <143503714+diamondstorm-leaderofchickenresistance@users.noreply.github.com>
2023-09-19 19:41:19 +0800

    Update javascriptforcoop.js

--- javascriptforcoop.js
+++ javascriptforcoop.js
@@ -3,7 +3,8 @@ const images = [
   "docs/assets/old fashion chicken.png",
   "docs/assets/tschika fetch the chips.png",
   "docs/assets/justice for shrimp.png",
-  "docs/assets/find your inner tschika.png"
+  "docs/assets/find your inner tschika.png",
+  "docs/assets/littoo tschika find about cybersaftey meme.png"
 ];
 var randomIndex = Math.floor(Math.random() * images.length);
 var selectedImage = images[randomIndex];
