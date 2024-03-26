a798b45d9cf134500d7aab53b280e1b2c62acaba
littootschika <143503714+littootschika@users.noreply.github.com>
2024-03-26 16:18:43 +0800

    Update javascriptforcoop.js

--- javascriptforcoop.js
+++ javascriptforcoop.js
@@ -1,4 +1,4 @@
-function ChangeBackground() {
+function changeBackground() {
 const images = [
   "docs/assets/old fashion chicken.png",
   "docs/assets/tschika fetch the chips.png",
@@ -11,16 +11,14 @@ const randomIndex = Math.floor(Math.random() * images.length);
 let selectedImage = images[randomIndex];
 document.body.style.backgroundImage = "url('" + selectedImage + "')";
 };
-
 function lockIdiots() {
   const keyboardLetters = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
   const passwordBox = document.getElementById('passwordbox');
   let passwordBoxInput = passwordBox.valueOf();
   try {
-    if (isNaN(passwordBoxInput) && passwordBoxInput !== 'zxcvbnm,./'){
-      throw "The password does not contain letters.\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n retard";
+    if (passwordBoxInput.includes(keyboardLetters) && passwordBoxInput !== "zxcvbnm,./"){
+      throw "The password does not contain letters";
     }
-    if (passwordBoxInput)
   }
   catch(err){
     alert(err);
